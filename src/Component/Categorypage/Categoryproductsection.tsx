7b755852a0e87c0dd6575dfc6144389d83112e84
Ram200231 <121919685+Ram200231@users.noreply.github.com>
2024-09-29 12:03:31 +0530

    Tech information Section Completion

--- src/Component/Categorypage/Categoryproductsection.tsx
+++ src/Component/Categorypage/Categoryproductsection.tsx
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import Heading from "../Common/Heading";
 import { Link } from "react-router-dom";
 import CategoryDatalist from '../../Assets/json/category.json';
-
+import Productdata from '../../Assets/json/product.json';
+import { productDataArray } from "../../Typescript/product";
 
 interface props {
     Brandname: string
@@ -43,29 +44,35 @@ class CategoryProductSection extends Component<props, {}> {
     render() {
         return (
             <div>
-                {/* Heading component */}
                 <Heading heading={this.props.Brandname} />
-
-                {/* Render the brand using map */}
                 <div className="product-list-cd">
-                    {Object.keys(CategoryDatalist)?.map((key) => {
-                        const item = (CategoryDatalist as CategoryData)[key];
-                        return (
+                {Object.keys(CategoryDatalist)?.map((key) => {
+    const item = (CategoryDatalist as CategoryData)[key];
+    const product = item?.product;
+    const productData = Productdata as productDataArray;
 
-                            <div key={key} className="product-item category-con">
-                                <div className="pd-top-container">
-                                    <img src={`${process.env.REACT_APP_BaseURL}${item.image}`} alt={item.name} className="product-img-c" />
-                                </div>
-                                <div className="pd-bottom-container">
-                                    <p className="productname">{item.name}</p>
-                                    <p className="product-description txt-ellipse">{item.Description.para?.[0]}</p>
-                                    <Link to={`/product/${key}`} className="view-details-btn">
-                                        <button type="button" className="text-center">View Details</button>
-                                    </Link>
-                                </div>
-                            </div>
-                        );
-                    })}
+    // Filter the productData to get only the products that are in the products array
+    const filteredProducts = product?.map(productId => productData[productId]).filter(Boolean);
+
+    // Return the mapped products JSX
+    return filteredProducts?.map((item_1, index) => {
+        return (
+            <div key={key} className="product-item category-con">
+                <div className="pd-top-container">
+                    <img src={`${process.env.REACT_APP_BaseURL}${item_1.productpreview}`} alt={item_1.colorName} className="product-img-c" />
+                </div>
+                <div className="pd-bottom-container">
+                    <p className="productname">{item_1.colorName}</p>
+                    <p className="product-description txt-ellipse">{item.Description.para?.[0]}</p>
+                    <Link to={`/product/${item_1.product_id}`} className="view-details-btn">
+                        <button type="button" className="text-center">View Details</button>
+                    </Link>
+                </div>
+            </div>
+        );
+    });
+})}
+
                 </div>
             </div>
         );
